6d7621026f4a7e8993a5660cc9664c9f2eff7fdb
Frostela <mainakdashlr@gmail.com>
2026-04-19 15:18:50 +0530

    Home page done, not responsive for moba yet

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Lenis Scroll ----------------------------------------------------------------------
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
+
+// Featured Properties Section -------------------------------------------------------------
+
 const properties = [
   {
     title: "Japanese Minka Townhouse",
